8dbcf655cd741241d224ce9269264caeb24ea08e
BurahH <75339032+BurahH@users.noreply.github.com>
2024-02-22 01:56:35 +0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,25 @@
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
+
+suspend fun main(args: Array<String>) = coroutineScope {
+    var urls = arrayOf("https://www.google.com", "https://www.facebook.com", "https://www.github.com", "https://www.twitter.com", "https://www.instagram.com",
+    "https://teach.vibelab.ru/", "https://vk.com/", "https://www.youtube.com/", "https://trello.com/", "https://lk.etu.ru/")
+    urls.forEach { url ->
+        launch {checkWebsite(url)}
+    }
+}
+
+fun checkWebsite(url: String) {
+    try {
+        val connection = URL(url).openConnection() as HttpURLConnection
+        connection.requestMethod = "HEAD"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+        connection.responseCode == HttpURLConnection.HTTP_OK
+        print("Сайт " +url+ "доступен\n")
+    } catch (e: Exception) {
+        print("Сайт " +url+ "недоступен\n")
+    }
+}
